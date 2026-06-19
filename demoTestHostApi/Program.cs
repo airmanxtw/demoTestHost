e27375365cd62c16173cbccaf97068e062d9465b
airmanxtw <airmanx@stust.edu.tw>
2026-06-19 10:47:35 +0800

    優化代碼格式，移除多餘的空行

--- demoTestHostApi/Program.cs
+++ demoTestHostApi/Program.cs
@@ -18,10 +18,9 @@ var summaries = new[]
 {
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
-
 app.MapGet("/weatherforecast", () =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
+    var forecast = Enumerable.Range(1, 5).Select(index =>
         new WeatherForecast
         (
             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
